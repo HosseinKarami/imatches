23572e037c33bea3abb439aa40e27e70e04b4e1f
Hossein Karami <athreepik@gmail.com>
2014-03-03 17:16:34 +0330

    Hello World

---
+++ Gruntfile.js
@@ -0,0 +1,274 @@
+/*!
+ * FireShell Gruntfile
+ * http://getfireshell.com
+ * @author Todd Motto
+ */
+
+'use strict';
+
+/**
+ * Livereload and connect variables
+ */
+var LIVERELOAD_PORT = 35729;
+var lrSnippet = require('connect-livereload')({
+  port: LIVERELOAD_PORT
+});
+
+/**
+ * Grunt module
+ */
+module.exports = function (grunt) {
+
+  /**
+   * Dynamically load npm tasks
+   */
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  /**
+   * FireShell Grunt config
+   */
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    /**
+     * Set project info
+     */
+    project: {
+      src: 'src',
+      app: '',
+      assets: 'assets',
+      css: [
+        '<%= project.src %>/scss/style.scss'
+      ],
+      js: [
+        '<%= project.src %>/js/*.js'
+      ]
+    },
+
+    /**
+     * Project banner
+     * Dynamically appended to CSS/JS files
+     * Inherits text from package.json
+     */
+    tag: {
+      banner: '/*!\n' +
+              ' * <%= pkg.name %>\n' +
+              ' * <%= pkg.title %>\n' +
+              ' * <%= pkg.url %>\n' +
+              ' * @author <%= pkg.author %>\n' +
+              ' * @version <%= pkg.version %>\n' +
+              ' * Copyright <%= pkg.copyright %>. <%= pkg.license %> licensed.\n' +
+              ' */\n'
+    },
+
+    jekyll: {
+        server: {
+            auto: true,
+            server: true,
+            server_port: 4040,
+            exclude: ['node_modules'],
+            dest: './_site'
+        }
+    },
+
+    /**
+     * JSHint
+     * https://github.com/gruntjs/grunt-contrib-jshint
+     * Manage the options inside .jshintrc file
+     */
+    jshint: {
+      files: ['src/js/*.js'],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    /**
+     * Concatenate JavaScript files
+     * https://github.com/gruntjs/grunt-contrib-concat
+     * Imports all .js files and appends project banner
+     */
+    concat: {
+      dev: {
+        files: {
+          '<%= project.assets %>/js/scripts.min.js': '<%= project.js %>'
+        }
+      },
+      options: {
+        stripBanners: true,
+        nonull: true,
+        banner: '<%= tag.banner %>'
+      }
+    },
+
+    /**
+     * Uglify (minify) JavaScript files
+     * https://github.com/gruntjs/grunt-contrib-uglify
+     * Compresses and minifies all JavaScript files into one
+     */
+    uglify: {
+      options: {
+        banner: "<%= tag.banner %>"
+      },
+      dist: {
+        files: {
+          '<%= project.assets %>/js/scripts.min.js': '<%= project.js %>'
+        }
+      }
+    },
+
+    /**
+     * Compile Sass/SCSS files
+     * https://github.com/gruntjs/grunt-contrib-sass
+     * Compiles all Sass/SCSS files and appends project banner
+     */
+    sass: {
+      dev: {
+        options: {
+          style: 'expanded',
+          banner: '<%= tag.banner %>'
+        },
+        files: {
+          '<%= project.assets %>/css/style.unprefixed.css': '<%= project.css %>'
+        }
+      },
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          '<%= project.assets %>/css/style.unprefixed.css': '<%= project.css %>'
+        }
+      }
+    },
+    /**
+     * Autoprefixer
+     * Adds vendor prefixes automatically
+     * https://github.com/nDmitry/grunt-autoprefixer
+     */
+    autoprefixer: {
+      options: {
+        browsers: [
+          'last 2 version',
+          'safari 6',
+          'ie 9',
+          'opera 12.1',
+          'ios 6',
+          'android 4'
+        ]
+      },
+      dev: {
+        files: {
+          '<%= project.assets %>/css/style.min.css': ['<%= project.assets %>/css/style.unprefixed.css']
+        }
+      },
+      dist: {
+        files: {
+          '<%= project.assets %>/css/style.prefixed.css': ['<%= project.assets %>/css/style.unprefixed.css']
+        }
+      }
+    },
+    /**
+     * CSSMin
+     * CSS minification
+     * https://github.com/gruntjs/grunt-contrib-cssmin
+     */
+    cssmin: {
+      dev: {
+        options: {
+          banner: '<%= tag.banner %>'
+        },
+        files: {
+          '<%= project.assets %>/css/style.min.css': [
+            '<%= project.assets %>/css/style.unprefixed.css'
+          ]
+        }
+      },
+      dist: {
+        options: {
+          banner: '<%= tag.banner %>'
+        },
+        files: {
+          '<%= project.assets %>/css/style.min.css': [
+            '<%= project.assets %>/css/style.prefixed.css'
+          ]
+        }
+      }
+    },
+    concurrent: {
+      target: {
+        tasks: ['jekyll:server', 'watch', 'open'],
+        options: {
+            logConcurrentOutput: true
+        }
+      }
+    },
+
+    /**
+     * Opens the web server in the browser
+     * https://github.com/jsoverson/grunt-open
+     */
+    open: {
+      server: {
+        path: 'http://localhost:4040'
+      }
+    },
+
+    /**
+     * Runs tasks against changed watched files
+     * https://github.com/gruntjs/grunt-contrib-watch
+     * Watching development files and run concat/compile tasks
+     * Livereload the browser once complete
+     */
+    watch: {
+      concat: {
+        files: '<%= project.src %>/js/{,*/}*.js',
+        tasks: ['concat:dev', 'jshint']
+      },
+      sass: {
+        files: '<%= project.src %>/scss/{,*/}*.{scss,sass}',
+        tasks: ['sass:dev', 'cssmin:dev', 'autoprefixer:dev']
+      },
+      livereload: {
+        options: {
+          livereload: true
+        },
+        files: [
+          '_config.yml',
+          '{,*/}*.{html, markdown}',
+          '<%= project.assets %>/css/*.css',
+          '<%= project.assets %>/js/{,*/}*.js',
+          '<%= project.assets %>/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    }
+  });
+
+  /**
+   * Default task
+   * Run `grunt` on the command line
+   */
+  grunt.registerTask('default', [
+    'sass:dev',
+    'cssmin:dev',
+    'autoprefixer:dev',
+    'jshint',
+    'concat:dev',
+    'concurrent:target'
+  ]);
+
+  /**
+   * Build task
+   * Run `grunt build` on the command line
+   * Then compress all JS/CSS files
+   */
+  grunt.registerTask('build', [
+    'sass:dist',
+    'autoprefixer:dist',
+    'cssmin:dist',
+    'jshint',
+    'uglify'
+  ]);
+
+};
---
+++ src/js/scripts.js
@@ -0,0 +1,6 @@
+(function (window, document, undefined) {
+
+  'use strict';
+
+
+})( window, document);
